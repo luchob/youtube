d5c844fcef4531eed701f2231f40fc2c05aacfb4
luchob <lachezar.balev@gmail.com>
2025-04-22 14:18:26 +0300

    Kafka demo.

---
+++ kafka-persistence/src/main/java/eu/balev/kafka/demo/web/KafkaDemoController.java
@@ -0,0 +1,29 @@
+package eu.balev.kafka.demo.web;
+
+import eu.balev.kafka.demo.service.KafkaDemoService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class KafkaDemoController {
+
+  private final KafkaDemoService kafkaDemoService;
+
+  public KafkaDemoController(KafkaDemoService kafkaDemoService) {
+    this.kafkaDemoService = kafkaDemoService;
+  }
+
+  @GetMapping("/create")
+  public String create() {
+    kafkaDemoService.createTestKafkaTopic();
+    return "created";
+  }
+
+  @GetMapping("/publish")
+  public String publish(@RequestParam("count") int count) {
+    kafkaDemoService
+        .publishMessages(count);
+    return "published";
+  }
+}
